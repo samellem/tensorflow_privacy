fe604629570bdc713371d32efbd174567e29ab9f
Stefan Mellem <mllm@google.com>
2022-05-19 19:24:25 -0700

    Bazel build in setup.py.

--- setup.py
+++ setup.py
@@ -12,9 +12,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """TensorFlow Privacy library setup file for pip."""
+import os
+import shutil
+import subprocess
+import sys
 
 from setuptools import find_packages
 from setuptools import setup
+
+
+class _BuildCommand(build.build):
+  """Build everything that is needed to install.
+
+  This overrides the original distutils "build" command to to run bazel_build
+  command before any sub_commands.
+
+  build command is also invoked from bdist_wheel and install command, therefore
+  this implementation covers the following commands:
+    - pip install . (which invokes bdist_wheel)
+    - python setup.py install (which invokes install command)
+    - python setup.py bdist_wheel (which invokes bdist_wheel command)
+  """
+
+  def _build_dp_accounting(self):
+    """Predicate method for building the DP accounting library."""
+    return True
+
+  # Add "bazel_build" command as the first sub_command of "build". Each
+  # sub_command of "build" (e.g. "build_py", "build_ext", etc.) is executed
+  # sequentially when running a "build" command, if the second item in the tuple
+  # (predicate method) is evaluated to true.
+  sub_commands = [
+      ('bazel_build', _build_dp_accounting),
+  ] + build.build.sub_commands
+
+
+class _BazelBuildCommand(setuptools.Command):
+  """Build library with Bazel.
+
+  Running this command will necessarily fetch external dependencies, including
+  the com_google_differential_py.
+  """
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    self._bazel_cmd = shutil.which('bazel')
+    if not self._bazel_cmd:
+      raise RuntimeError(
+          'Could not find "bazel" binary. Please visit '
+          'https://docs.bazel.build/versions/master/install.html for '
+          'installation instruction.')
+
+  def run(self):
+    subprocess.check_call(
+        [self._bazel_cmd, 'run', '-c', 'opt'] +
+        ['//...'],
+        # Bazel should be invoked in a directory containing bazel WORKSPACE
+        # file, which is the root directory.
+        cwd=os.path.dirname(os.path.realpath(__file__)),
+        env=dict(os.environ, PYTHON_BIN_PATH=sys.executable))
+
 
 with open('tensorflow_privacy/version.py') as file:
   globals_dict = {}
@@ -40,4 +99,8 @@ setup(
         'tensorflow-probability~=0.15.0',
         'tensorflow~=2.4',
     ],
+    cmdclass={
+        'build': _BuildCommand,
+        'bazel_build', _BazelBuildCommand,
+    },
     packages=find_packages())
